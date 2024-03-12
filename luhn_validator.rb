bd1a5f4f09bbff897c2cd89c337ae77a891228b8
楊誼萱 <ella5898g5@gmail.com>
2024-03-13 02:22:08 +0800

    implement the validate_checksum method done

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,15 +5,17 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-    # TODO: use the integers in nums_a to validate its last check digit
-    # Double every second digit from the right
-    nums_a.reverse.each_with_index { |digit, index| nums_a[index] *= 2 if index.odd? }
-    # Sum the nums_a of the numbers (if any are double nums_a)
-    sum = nums_a.map { |digit| digit > 9 ? digit - 9 : digit }.sum
-    # Check if the sum is divisible by 10
-    sum % 10 == 0
+
+    # Double every second digit from the right, adjust digits if greater than 9, and sum them up
+    sum = nums_a.reverse.map.with_index do |digit, index|
+      digit *= 2 if index.odd?
+      digit -= 9 if digit > 9
+      digit
+    end.sum
+
+    # Check if the sum % 10 == 0
+    (sum % 10)==0
   end
 end
-
 
 
