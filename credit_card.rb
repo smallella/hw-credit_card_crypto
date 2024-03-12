2c6499f0f9b86d3c90699ca514c1a264a768c89b
楊誼萱 <ella5898g5@gmail.com>
2024-03-13 01:07:36 +0800

    CreditCard Class Implementation

--- credit_card.rb
+++ credit_card.rb
@@ -3,18 +3,26 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
+  def to_json(*_args)
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      @number = number
+      @expiration_date = expiration_date
+      @owner = owner
+      @credit_network = credit_network
     }.to_json
   end
 
@@ -26,6 +34,8 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    data = JSON.parse(card_s)
+    new(data['number'], data['expiration_date'], data['owner'], data['credit_network'])
   end
 
   # return a hash of the serialized credit card object
@@ -34,6 +44,8 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    # Serialize the credit card object and hash its contents
+    to_json.hash
   end
 
   # return a cryptographically secure hash
@@ -41,5 +53,8 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    Digest::SHA256.hexdigest(to_json)
   end
 end
+
+
